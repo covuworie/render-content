ba9b51a0158a11347aaec37e443d352024f40d1b
Chuk Ovuworie <chuk.ovuworie@dotevoke.com>
2020-04-21 12:47:37 -0500

    Add code for rendering with DOM API

---
+++ src/dom-api/src/legend.ts
@@ -0,0 +1,13 @@
+export interface LegendInterface {
+    name: string;
+    nationality: string;
+    position: string;
+}
+
+export type LegendInterfaceKeys = keyof LegendInterface;
+
+export class Legend implements LegendInterface {
+    constructor(public name: string, public nationality: string, public position: string) {
+
+    }
+}
